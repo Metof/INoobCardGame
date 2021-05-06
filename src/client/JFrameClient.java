bc1903471f960940b2d0a8518fc2274af01dba17
Metof <quiquoqua2000>
2021-05-06 12:27:19 +0200

    setup

--- src/client/JFrameClient.java
+++ src/client/JFrameClient.java
@@ -3,30 +3,49 @@ package client;
 import client.Game.RoomPage;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class JFrameClient extends JFrame {
+    private JPanel mainPanel = new JPanel();
 
     public JFrameClient(){
         super("INoobCardGame");
         this.setLayout(null);
-        this.setSize(600, 600);
+        this.setPreferredSize(new Dimension(600, 600));
+        this.pack();
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initFrame();
         goToStartingPage();
     }
-    public void goToStartingPage(){
-        this.removeAll();
-        this.add(new StartingPage(this.getSize()));
+
+    public void initFrame(){
+        this.add(mainPanel);
         this.setVisible(true);
+    }
+
+    public void goToStartingPage(){
+        /*
+        this.mainPanel.setLayout(null);
+        this.mainPanel.setSize(this.getSize());
+        JButton g = new JButton("Staring page");
+        g.setSize(100, 100);
+        this.mainPanel.add(g);
+        g.setVisible(true);
+        */
+
+        this.mainPanel = new StartingPage(this.getSize());
+        this.mainPanel.setSize(this.getSize());
+        this.mainPanel.repaint();
+        this.mainPanel.revalidate();
+
+        this.repaint();
+        this.revalidate();
     }
 
     public void goToSettingsRoomPage(RoomPage settingsRoom){
-        this.removeAll();
-        this.setVisible(true);
     }
 
     public void goToGameRoomPage(){
-        this.removeAll();
-        this.setVisible(true);
     }
 }
--- src/client/StartingPage.java
+++ src/client/StartingPage.java
@@ -5,6 +5,24 @@ import java.awt.*;
 
 public class StartingPage extends JPanel {
     public StartingPage(Dimension d){
+        this.setLayout(null);
+        this.setSize(this.getSize());
+        JButton g = new JButton("Staring page");
+        g.setSize(100, 100);
+        this.add(g);
+        g.setVisible(true);
+        /*
+        this.setLayout(null);
+        this.setSize(d);
+        //this.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+        JButton g = new JButton("Staring page");
+        g.setSize(100, 100);
+        this.add(g);
+        g.setVisible(true);
+        this.setVisible(true);
+        this.repaint();
+        this.revalidate();
 
+         */
     }
 }
